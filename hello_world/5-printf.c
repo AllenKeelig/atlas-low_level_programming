dba151869e159f039bc2d6643c6d9aaf757c30e4
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-18 15:00:19 +0000

    5.0

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a piece of art with proper grammar
+ *              using the printf function and returns 0 to indicate success.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
+
