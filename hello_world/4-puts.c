a6f4a706b97040c1a54513ba98d701caa10aa04b
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-18 14:45:31 +0000

    4.2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
-int main() {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a specified string and
+ *              returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
 
