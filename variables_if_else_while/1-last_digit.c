ec18d5487944885ed9b1c0f6fa53eaea74730bbd
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-19 05:32:20 +0000

    1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-    int n;
+	int n;
     int lastDigit;
 
     srand(time(0));
@@ -23,15 +23,15 @@ int main(void)
     /* Check and print the condition */
     if (lastDigit > 5)
     {
-        printf("greater than 5\n");
+	    printf("greater than 5\n");
     }
     else if (lastDigit == 0)
     {
-        printf("0\n");
+	    printf("0\n");
     }
     else
     {
-        printf("less than 6 and not 0\n");
+	    printf("less than 6 and not 0\n");
     }
 
     return (0);
