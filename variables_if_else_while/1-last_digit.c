48cf44eb1948b11d7686476d877904b21abde694
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-19 04:58:40 +0000

    1.3

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,31 +12,23 @@
  */
 int main(void)
 {
-        int n, lastDigit;
-
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-
-        printf("%d is \n", n);
-
-        /* Extracting the last digit using modulo 10 */
-        lastDigit = n % 10;
-
-        printf("%d and is ", lastDigit);
-
-        if (lastDigit > 5)
-        {
-                printf("greater than 5\n");
-        }
-        else if (lastDigit == 0)
-        {
-                printf("0\n");
-        }
-        else
-        {
-                printf("less than 6 and not 0\n");
-        }
-
-        return (0);
+	int n, lastdigit;
+	srand(time(0)):
+		n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is \n", n);
+	lastDigit = n % 10;
+	printf("%d and is ", lastDigit);
+	if (lastDigit > 5)
+	{
+		printf("greater than 5\n");
+	}
+	else if (lastDigit == 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		printf("less than 6 and not 0\n");
+	}
+	return (0);
 }
-
