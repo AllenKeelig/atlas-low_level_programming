b8daf736c5f7c81e7edab41ba5d87a971e18224c
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-19 05:08:39 +0000

    1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,23 +12,25 @@
  */
 int main(void)
 {
-	int n, lastDigit;
-	srand(time(0));
-		n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d and is ", n, lastDigit);
-	lastDigit = n % 10;
-	printf("%d and is ", lastDigit);
-	if (lastDigit > 5)
-	{
-		printf("greater than 5\n");
-	}
-	else if (lastDigit == 0)
-	{
-		printf("0\n");
-	}
-	else
-	{
-		printf("less than 6 and not 0\n");
-	}
-	return (0);
+    int n, lastDigit;  // Corrected the variable name 'lastdigit' to 'lastDigit'
+    srand(time(0));    // Corrected the colon to a semicolon
+
+    n = rand() - RAND_MAX / 2;
+    printf("Last digit of %d is %d and is ", n, lastDigit);  // Added the missing '%d' for lastDigit
+    lastDigit = n % 10;
+
+    if (lastDigit > 5)
+    {
+        printf("greater than 5\n");
+    }
+    else if (lastDigit == 0)
+    {
+        printf("0\n");
+    }
+    else
+    {
+        printf("less than 6 and not 0\n");
+    }
+    return (0);
 }
+
