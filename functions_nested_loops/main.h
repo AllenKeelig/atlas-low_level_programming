b7a97185d13d851fc20f6580881ebe7ef4e6a739
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-20 04:24:11 +0000

    3.1

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int _putchar(char c);
 /* Function prototypes for print_alphabet and print_alphabet_x10 */
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 
 #endif /* MAIN_H */
 
