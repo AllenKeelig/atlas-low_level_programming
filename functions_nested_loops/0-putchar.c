00ee3c82652e22099e31fad527662395c2763d32
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-20 01:02:20 +0000

    0.3

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main() {
+/**
+ * main - Entry point
+ *
+ * Description: Prints "_putchar" followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	printf("_putchar\n");
-	return 0;
+	return (0);
 }
 
