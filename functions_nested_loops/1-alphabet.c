61bcdadb8b879c41a5e844f631dddeadf089f659
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-20 01:22:58 +0000

    1.1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
-void _putchar(char c) {
-	putchar(c);
-}
+void _putchar(char c);
 
 /**
  * print_alphabet - Prints the alphabet in lowercase followed by a new line
  */
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void _putchar(char c)
+{
+	putchar(c);
+}
+
 void print_alphabet(void)
 {
 	char letter = 'a';
@@ -19,10 +30,3 @@ void print_alphabet(void)
 
 	_putchar('\n');
 }
-
-int main(void)
-{
-	print_alphabet();
-		return (0);
-}
-
