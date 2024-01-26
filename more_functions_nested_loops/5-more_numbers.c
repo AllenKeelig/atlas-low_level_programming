c98e7a3fb5d162684af20d15d0ac04e5a25fb76c
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-26 17:25:49 +0000

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ */
+void more_numbers(void)
+{
+    int i, j;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j <= 14; j++)
+        {
+            if (j > 9)
+            {
+                /* If the number is two digits, print the first digit */
+                _putchar('0' + j / 10);
+            }
+            /* Print the last digit */
+            _putchar('0' + j % 10);
+        }
+        /* Print a new line after each set of numbers */
+        _putchar('\n');
+    }
+}
+
