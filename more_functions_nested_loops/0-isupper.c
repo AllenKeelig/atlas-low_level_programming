552c5bc14578598ce4cb6fc74613ed4c66168e21
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-26 10:13:07 +0000

    0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,25 @@
-#include "main.h"
 #include <stdio.h>
 
-int _isdigit(int c) {
+/**
+ * _isdigit - Checks if a character is a digit (0 through 9).
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is a digit, 0 otherwise.
+ */
+int _isdigit(int c)
+{
     return (c >= '0' && c <= '9') ? 1 : 0;
 }
 
-int main() {
-    // Test cases
-    printf("%d\n", _isdigit('5'));  // Should print 1
-    printf("%d\n", _isdigit('a'));  // Should print 0
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+    printf("%d\n", _isdigit('5'));
+    printf("%d\n", _isdigit('a'));
 
     return 0;
 }
