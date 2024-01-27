1ff640750d0595f8c6d799ea9fcea837c026be56
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-27 23:20:36 +0000

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,31 +8,26 @@
  */
 int _atoi(char *s)
 {
-    int result = 0;
     int sign = 1;
+    int result = 0;
     int i = 0;
 
-    /* Handle leading whitespaces */
-    while (s[i] == ' ')
+    while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
         i++;
 
-    /* Handle sign */
-    if (s[i] == '-' || s[i] == '+') {
-        sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '-')
+    {
+        sign = -1;
+        i++;
+    }
+    else if (s[i] == '+')
+    {
         i++;
     }
 
-    /* Convert digits to integer */
-    while (s[i] >= '0' && s[i] <= '9') {
-        /* Check for overflow before updating the result */
-        int digit = s[i] - '0';
-
-        if ((result > INT_MAX / 10) || ((result == INT_MAX / 10) && (digit > INT_MAX % 10))) {
-            /* Overflow occurred, handle appropriately */
-            return (sign == 1) ? INT_MAX : INT_MIN;
-        }
-
-        result = result * 10 + digit;
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        result = result * 10 + (s[i] - '0');
         i++;
     }
 
