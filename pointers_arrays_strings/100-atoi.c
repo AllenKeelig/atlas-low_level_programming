63d6eaeb9a5bf3caca778a8c68c86dad9dbdf03d
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-27 23:14:23 +0000

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,40 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-int _atoi(char *s);
-
-int main() {
-    const char *str = "12345";
-    int num = _atoi(str);
-
-    printf("Converted number: %d\n", num);
-
-    return 0;
+int _atoi(char *s) 
+{
 }
-
-int _atoi(char *s) {
-    int result = 0;
-    int sign = 1; /* 1 for positive, -1 for negative */
-
-    /* Skip leading whitespaces */
-    while (*s == ' ' || (*s >= '\t' && *s <= '\r')) {
-        s++;
-    }
-
-    /* Check for the sign */
-    if (*s == '-') {
-        sign = -1;
-        s++;
-    } else if (*s == '+') {
-        s++;
-    }
-
-    /* Process digits */
-    while (*s >= '0' && *s <= '9') {
-        result = result * 10 + (*s - '0');
-        s++;
-    }
-
-    return result * sign;
-}
-
