0b9157949f43b853a95a7de1202cf1ad7eb1f6d8
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-27 16:52:47 +0000

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: Pointer to the input string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	/* Initialize length counter */
+	int length = 0;
+
+	/* Iterate through the characters of the input string */
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	/* Return the total length of the string */
+	return length;
+}
+
