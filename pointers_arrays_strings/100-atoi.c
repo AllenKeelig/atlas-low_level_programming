d9d2d4c2892b71dc91ab3dccc1731e0379b8e183
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-27 23:05:58 +0000

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,14 +1,29 @@
-#include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 
-/**
- * _atoi - Converts a string to an integer.
- * @s: The input string.
- *
- * Return: The converted integer.
- */
-int _atoi(char *s)
-{
-    return atoi(s);
+int atoi(const char *str) {
+    int result = 0;
+    int sign = 1; // 1 for positive, -1 for negative
+
+    // Skip leading whitespaces
+    while (*str == ' ' || (*str >= '\t' && *str <= '\r')) {
+        str++;
+    }
+
+    // Check for the sign
+    if (*str == '-') {
+        sign = -1;
+        str++;
+    } else if (*str == '+') {
+        str++;
+    }
+
+    // Process digits
+    while (*str >= '0' && *str <= '9') {
+        result = result * 10 + (*str - '0');
+        str++;
+    }
+
+    return result * sign;
 }
+
 
