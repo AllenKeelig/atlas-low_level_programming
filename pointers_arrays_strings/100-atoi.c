1543ce5f67c1d079d6da0ca271320c36b67ab20d
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-27 23:16:45 +0000

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,41 @@
-#include <stdio.h>
 #include "main.h"
 
-int _atoi(char *s) 
+/**
+ * _atoi - Convert a string to an integer.
+ * @s: The input string.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
 {
+    int result = 0;
+    int sign = 1;
+    int i = 0;
+
+    /* Handle leading whitespaces */
+    while (s[i] == ' ')
+        i++;
+
+    /* Handle sign */
+    if (s[i] == '-' || s[i] == '+') {
+        sign = (s[i] == '-') ? -1 : 1;
+        i++;
+    }
+
+    /* Convert digits to integer */
+    while (s[i] >= '0' && s[i] <= '9') {
+        /* Check for overflow before updating the result */
+        int digit = s[i] - '0';
+
+        if ((result > INT_MAX / 10) || ((result == INT_MAX / 10) && (digit > INT_MAX % 10))) {
+            /* Overflow occurred, handle appropriately */
+            return (sign == 1) ? INT_MAX : INT_MIN;
+        }
+
+        result = result * 10 + digit;
+        i++;
+    }
+
+    return sign * result;
 }
+
