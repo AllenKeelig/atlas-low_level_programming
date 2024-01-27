e2cea9f2e557629c3338746ed1212521b0d2579e
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-27 10:58:32 +0000

    9-spacesandanewline?

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,23 @@ void fizz_buzz(void)
 	{
 		if (digit % 3 == 0 && digit % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (digit % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (digit % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", digit);
+			printf("%d", digit);
+		}
+		if (digit < 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
