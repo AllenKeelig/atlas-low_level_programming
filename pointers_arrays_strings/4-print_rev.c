c40b2ad0127ecb19577c58f0bf07132c3facde91
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-27 17:07:37 +0000

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse followed by a new line
+ * @s: Pointer to the input string
+ *
+ * Description: This function calculates the length of the input string,
+ *              then prints the characters in reverse order followed by a new line.
+ */
+void print_rev(char *s)
+{
+	/* Calculate the length of the string */
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	/* Print the characters in reverse order */
+	for (int i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	/* Print a new line at the end */
+	_putchar('\n');
+}
+
