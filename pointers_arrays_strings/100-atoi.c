0da6344b7c5ef435a126f9bfcda0a6dc4f2ffb51
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-28 14:39:50 +0000

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <limits.h>
-#include <ctype.h>
 
 /**
  * _atoi - Convert a string to an integer.
@@ -10,37 +9,30 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    long result = 0;
-    int i = 0;
-    int digit_found = 0;  /* Variable to track if digits are found */
+	int sign = 1;
+	long result = 0;
+	int i = 0;
 
-    /* Skip leading whitespaces, signs, and non-digit characters */
-    while (isspace(s[i]) || (s[i] == '-' || s[i] == '+') || !isdigit(s[i]))
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        if (isdigit(s[i]))
-            digit_found = 1;  /* Set to 1 if digits are found */
-        i++;
-    }
+	/* Skip leading whitespaces and non-digit characters */
+	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13) || (s[i] == '-' || s[i] == '+'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
 
-    /* If no digits are found, return 0 */
-    if (digit_found == 0)
-        return 0;
+	/* Process digits and build the result */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
 
-    /* Process digits and build the result */
-    while (isdigit(s[i]))
-    {
-        result = result * 10 + (s[i] - '0');
+		/* Check for overflow or underflow */
+		if ((sign == 1 && result > INT_MAX) || (sign == -1 && -result < INT_MIN))
+			return sign == 1 ? INT_MAX : INT_MIN;
 
-        /* Check for overflow or underflow */
-        if ((sign == 1 && result > INT_MAX) || (sign == -1 && -result < INT_MIN))
-            return (sign == 1 ? INT_MAX : INT_MIN);
+		i++;
+	}
 
-        i++;
-    }
-
-    return (sign * (int)result);
+	return sign * (int)result;
 }
 
