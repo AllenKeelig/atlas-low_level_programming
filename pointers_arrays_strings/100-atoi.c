eead0438339e02621779ab10fce70b750219b904
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-28 14:47:08 +0000

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,38 +1,39 @@
 #include "main.h"
 #include <limits.h>
+#include <ctype.h>
 
 /**
  * _atoi - Convert a string to an integer.
  * @s: The input string.
  *
- * Return: The converted integer.
+ * Return: The converted integer, or 0 if no valid integer is found.
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	long result = 0;
-	int i = 0;
+    int sign = 1;
+    long result = 0;
+    int i = 0;
 
-	/* Skip leading whitespaces and non-digit characters */
-	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13) || (s[i] == '-' || s[i] == '+'))
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		i++;
-	}
+    /* Skip leading whitespaces, signs, and non-digit characters */
+    while (isspace(s[i]) || (s[i] == '-' || s[i] == '+'))
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        i++;
+    }
 
-	/* Process digits and build the result */
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
+    /* Process digits and build the result */
+    while (isdigit(s[i]))
+    {
+        result = result * 10 + (s[i] - '0');
 
-		/* Check for overflow or underflow */
-		if ((sign == 1 && result > INT_MAX) || (sign == -1 && -result < INT_MIN))
-			return sign == 1 ? INT_MAX : INT_MIN;
+        /* Check for overflow or underflow */
+        if ((sign == 1 && result > INT_MAX) || (sign == -1 && -result < INT_MIN))
+            return (sign == 1 ? INT_MAX : INT_MIN);
 
-		i++;
-	}
+        i++;
+    }
 
-	return sign * (int)result;
+    return (sign * (int)result);
 }
 
