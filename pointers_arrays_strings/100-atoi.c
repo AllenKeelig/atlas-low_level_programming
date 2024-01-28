9e265a36af89dc17103bfaad6a3ebf1e4a608984
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-28 01:08:16 +0000

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,29 +9,29 @@
 int _atoi(char *s)
 {
 	int sign = 1;
-	long result = 0;  /* Change the result type to long */
+	long result = 0;
 	int i = 0;
 
-	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
-		i++;
-
-	while (s[i] == '-' || s[i] == '+')
+	/* Skip leading whitespaces and non-digit characters */
+	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13) || (s[i] == '-' && s[i + 1] >= '0' && s[i + 1] <= '9'))
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		i++;
 	}
 
+	/* Process digits and build the result */
 	while (s[i] >= '0' && s[i] <= '9')
 	{
 		result = result * 10 + (s[i] - '0');
 
-		/* Check for overflow or underflow (optional) */
-		/* if ((sign == 1 && result > INT_MAX) || (sign == -1 && -result < INT_MIN))
-			return sign == 1 ? INT_MAX : INT_MIN; */
+		/* Check for overflow or underflow */
+		if ((sign == 1 && result > INT_MAX) || (sign == -1 && -result < INT_MIN))
+			return sign == 1 ? INT_MAX : INT_MIN;
 
 		i++;
 	}
 
-	return sign * (int)result;  /* Cast the result back to int before returning */
+	return sign * (int)result;
 }
+
