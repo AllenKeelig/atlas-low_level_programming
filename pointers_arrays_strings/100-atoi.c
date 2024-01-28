9d35814f7a0cb314e69b2d601e388183d7ab7952
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-28 01:02:03 +0000

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,26 +8,30 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    long result = 0;  /* Change the result type to long */
-    int i = 0;
+	int sign = 1;
+	long result = 0;  /* Change the result type to long */
+	int i = 0;
 
-    while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
-        i++;
+	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
+		i++;
 
-    while (s[i] == '-' || s[i] == '+')
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        i++;
-    }
+	while (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
 
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
 
-    return sign * (int)result;  /* Cast the result back to int before returning */
+		/* Check for overflow or underflow (optional) */
+		/* if ((sign == 1 && result > INT_MAX) || (sign == -1 && -result < INT_MIN))
+			return sign == 1 ? INT_MAX : INT_MIN; */
+
+		i++;
+	}
+
+	return sign * (int)result;  /* Cast the result back to int before returning */
 }
-
