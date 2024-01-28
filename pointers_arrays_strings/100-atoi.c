a75ff7f6d2995ba670c90f3ff96498a6d856b63d
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-28 12:56:00 +0000

    20

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,10 +12,9 @@ int _atoi(char *s)
 	int sign = 1;
 	long result = 0;
 	int i = 0;
-	int digit_count = 0; /* Track the number of consecutive digits */
 
 	/* Skip leading whitespaces and non-digit characters */
-	while (s[i] && (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13) || s[i] == '-' || s[i] == '+'))
+	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13) || (s[i] == '-' || s[i] == '+'))
 	{
 		if (s[i] == '-')
 			sign *= -1;
@@ -23,17 +22,9 @@ int _atoi(char *s)
 	}
 
 	/* Process digits and build the result */
-	while (s[i] && ((s[i] >= '0' && s[i] <= '9') || (s[i] == '-' && digit_count == 0)))
+	while (s[i] >= '0' && s[i] <= '9')
 	{
-		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			result = result * 10 + (s[i] - '0');
-			digit_count++;
-		}
-		else
-			break; /* Stop on encountering non-digit characters */
+		result = result * 10 + (s[i] - '0');
 
 		/* Check for overflow or underflow */
 		if ((sign == 1 && result > INT_MAX) || (sign == -1 && -result < INT_MIN))
