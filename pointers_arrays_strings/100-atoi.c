0b49ab0967696a62d1e24d9d71f554d70c1113b2
AllenKeelig <allen.keeling@atlasschool.com>
2024-01-28 14:26:42 +0000

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -14,12 +14,16 @@ int _atoi(char *s)
     long result = 0;
     int i = 0;
 
-    /* Skip leading whitespaces and signs */
-    while (s[i] && (isspace(s[i]) || s[i] == '-' || s[i] == '+'))
+    /* Skip leading whitespaces and signs, continue only if there are digits */
+    while (isspace(s[i]) || (s[i] == '-' || s[i] == '+') || !isdigit(s[i]))
     {
         if (s[i] == '-')
             sign *= -1;
         i++;
+
+        // If end of string is reached and no digits are encountered, break the loop
+        if (s[i] == '\0' && !isdigit(s[i - 1]))
+            break;
     }
 
     /* Process digits and build the result */
