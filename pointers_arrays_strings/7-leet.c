4f3e19b2a3060958deee138c15b436b569307489
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-03 03:52:47 +0000

    7-

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -10,27 +10,33 @@
  */
 char *leet(char *input)
 {
+	char *result;
+	size_t len;
+	size_t i
 
-    size_t len = strlen(input);
-    char *result = (char *)malloc((len + 1) * sizeof(char));
-    size_t i;
+	len = strlen(input);
+	result = (char *)malloc((len + 1) * sizeof(char));
 
-    for (i = 0; i < len; i++)
-    {
-        if (input[i] == 'a' || input[i] == 'A')
-            result[i] = '4';
-        else if (input[i] == 'e' || input[i] == 'E')
-            result[i] = '3';
-        else if (input[i] == 'o' || input[i] == 'O')
-            result[i] = '0';
-        else if (input[i] == 't' || input[i] == 'T')
-            result[i] = '7';
-        else if (input[i] == 'l' || input[i] == 'L')
-            result[i] = '1';
-        else
-            result[i] = input[i];
-    }
+	for (i = 0; i < len; i++)
+	{
+		if (input[i] == 'a' || input[i] == 'A' ||
+			input[i] == 'e' || input[i] == 'E' ||
+			input[i] == 'o' || input[i] == 'O' ||
+			input[i] == 't' || input[i] == 'T' ||
+			input[i] == 'l' || input[i] == 'L')
+		{
+			result[i] = (input[i] == 'a' || input[i] == 'A') ? '4' :
+						(input[i] == 'e' || input[i] == 'E') ? '3' :
+						(input[i] == 'o' || input[i] == 'O') ? '0' :
+						(input[i] == 't' || input[i] == 'T') ? '7' :
+						(input[i] == 'l' || input[i] == 'L') ? '1' : input[i];
+		}
+		else
+		{
+			result[i] = input[i];
+		}
+	}
 
-    result[len] = '\0';
-    return result;
+	result[len] = '\0';
+	return (result);
 }
