dbc8695cefc42441684790a9be919f1b06721125
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-03 23:32:17 +0000

    7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *leet(const char *input) {
+char *leet(const char *input) 
+{
 	size_t len = strlen(input);
 	char *result = (char *)malloc((len + 1) * sizeof(char));
-
+	size_t i
 	char *vowels_and_letters = "aeotlAEOTL";
 	char *leet_replacements = "4307143071";
 
-	for (size_t i = 0; i < len; i++) {
+	for (i = 0; i < len; i++) {
 		char *pos = strchr(vowels_and_letters, input[i]);
 
 		result[i] = (pos != NULL) ? leet_replacements[pos - vowels_and_letters] : input[i];
