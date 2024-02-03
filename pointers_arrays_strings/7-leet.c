4a9d937d2e9d475cf87f4d2e3ec43524db729550
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-03 23:23:29 +0000

    7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -2,32 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * leet - Encodes a string into 1337.
- * @input: The input string to be encoded.
- *
- * Return: A dynamically allocated string containing the encoded result.
- */
-char *leet(char *input)
-{
-	char *result;
-	size_t len, i;
+char *leet(const char *input) {
+	size_t len = strlen(input);
+	char *result = (char *)malloc((len + 1) * sizeof(char));
+
 	char *vowels_and_letters = "aeotlAEOTL";
-	char *leet_replacements = "2307143071"; 
+	char *leet_replacements = "4307143071";
 
-	len = strlen(input);
-	result = (char *)malloc((len + 1) * sizeof(char));
-
-	for (i = 0; i < len; i++)
-	{
+	for (size_t i = 0; i < len; i++) {
 		char *pos = strchr(vowels_and_letters, input[i]);
 
-		result[i] = input[i];
-		if (pos != NULL)
-		{
-			int index = pos - vowels_and_letters;
-			result[i] = leet_replacements[index];
-		}
+		result[i] = (pos != NULL) ? leet_replacements[pos - vowels_and_letters] : input[i];
 	}
 
 	result[len] = '\0';
