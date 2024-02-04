e5ec2d05b1cc72b556647305ae73de49e7190a5d
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-04 21:08:23 +0000

    0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,7 +1,8 @@
 #include <stddef.h>
 
+unsigned int i;
 char *_memset(char *s, char b, unsigned int n) {
-    for (unsigned int i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         s[i] = b;
     }
 
