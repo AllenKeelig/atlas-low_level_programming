39c75ead75e9b88c4a831b3ba8d9de1ee16b3a4c
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-09 21:08:34 +0000

    0betty

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,15 +2,24 @@
 #include "main.h"
 #include <string.h>
 
-
+/**
+ * _puts_recursion - Prints a string using recursion.
+ * @s: The string to be printed.
+ */
 void _puts_recursion(char *s)
 {
-	size_t length = strlen(s);	
+	/* Calculate the length of the string */
+	size_t length = strlen(s);
+
+	/* Base case: if the length is 0, print a newline and return */
 	if (length == 0)
 	{
 		putchar('\n');
 		return;
 	}
+
+	/* Print the current character and move to the next character recursively */
 	putchar(*s);
 	_puts_recursion(s + 1);
 }
+
