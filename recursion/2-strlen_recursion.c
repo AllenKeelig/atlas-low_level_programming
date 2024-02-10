65fe65cb4072b80fdc8bbd3ebb83261e2832876a
AllenKeelig <allen.keeling@atlasschools.com>
2024-02-10 16:02:36 +0000

    2btty?

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,10 +2,17 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * _strlen_recursion - Calculate the length of a string using recursion
+ * @s: The input string
+ *
+ * Return: The length of the string
+ */
+
 int _strlen_recursion(char *s)
 {
 	/* Calculate the length of the string */
 	size_t length = strlen(s);
-	return (int)length;
+	return ((int)length);
 }
 
