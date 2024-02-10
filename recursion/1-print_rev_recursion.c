a6da5d77c2606b698a394e432abb8d890f028f20
AllenKeelig <allen.keeling@atlasschools.com>
2024-02-10 15:13:32 +0000

    1firsttry?

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _print_rev_recursion - Prints a reversed string using recursion.
+ * @s: The string to be printed.
+ */
+void _print_rev_recursion(char *s);
+{
+        /* Calculate the length of the string */
+        size_t length = strlen(s);
+
+        /* Base case: if the length is 0, print a newline and return */
+        if (length == 0)
+        {
+                putchar('\n');
+                return;
+        }
+
+        /* Only change from last */
+	s += length - 1;
+        putchar(*s);
+        _print_rev_recursion(s + 1);
+}
