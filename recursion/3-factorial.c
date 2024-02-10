e138d0a9e57cb4cb3d8e7f701102c333dc9ca165
AllenKeelig <allen.keeling@atlasschools.com>
2024-02-10 16:15:47 +0000

    3neg

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
-int factorial(int n) {
-	if (n == 0 || n == 1) {
+int factorial(int n) 
+{
+	if (n < 0) 
+	{
+        return -1;
+	}
+	else if (n == 0 || n == 1)
+	{
 		return 1;
-	} else {
+	}
+	else
+	{
 		return n * factorial(n - 1);
 	}
 }
