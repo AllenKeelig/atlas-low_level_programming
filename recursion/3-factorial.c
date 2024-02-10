c8071eb425f00a1be38566b710b53c8664aeca00
AllenKeelig <allen.keeling@atlasschools.com>
2024-02-10 16:17:39 +0000

    3neg

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-int factorial(int n) 
+/**
+ * factorial - Calculates the factorial of a number.
+ * @n: The number for which the factorial is calculated.
+ *
+ * Return: The factorial of the given number, or -1 if n is negative.
+ */
+int factorial(int n)
 {
-	if (n < 0) 
+	if (n < 0)
 	{
-        return -1;
+		return -1;
 	}
 	else if (n == 0 || n == 1)
 	{
@@ -12,6 +18,7 @@ int factorial(int n)
 	}
 	else
 	{
-		return n * factorial(n - 1);
+		return (n * factorial(n - 1));
 	}
 }
+
