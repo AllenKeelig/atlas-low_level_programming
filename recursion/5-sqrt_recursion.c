8672b6eecb6ca203957b588c8f953893088d6303
AllenKeelig <allen.keeling@atlasschools.com>
2024-02-10 17:14:36 +0000

    5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include "main.h"
-#include <math.h>
+
+int custom_sqrt(int n, int guess) 
+{
+	if (n == 0 || n == 1)
+	{
+		return n;
+	}
+
+	if (guess * guess == n || (guess * guess < n && ((guess + 1) * (guess + 1) > n)))
+	{
+		return guess;
+	}
+
+	return custom_sqrt(n, (n / guess + guess) / 2);
+}
 
 int _sqrt_recursion(int n)
 {
-	int result = (int)sqrt((double)n);
-	return (result);
+	int result = custom_sqrt(n, n / 2);
+	return result;
 }
+
