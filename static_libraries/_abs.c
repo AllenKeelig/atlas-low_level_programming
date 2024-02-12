7eb0aca2ab4bbb178a621c8212f81e31630270cd
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-12 20:11:21 +0000

    0

--- static_libraries/_abs.c
+++ static_libraries/_abs.c
@@ -1,2 +1,13 @@
 #include "main.h"
-int _abs(int n);
+
+/**
+ * _abs - computes the absolute value of an integer.
+ * @n: The integer for which absolute value is computed.
+ *
+ * Return: The absolute value of n.
+ */
+int _abs(int n)
+{
+	return (n >= 0 ? n : -n);
+}
+
---
+++ static_libraries/_puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a specified string and
+ *              returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
