14f0177d4f8e70339215b0e365f53de46d5691d3
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-16 01:06:56 +0000

    0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -9,19 +9,22 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *newArray;
+	unsigned int i;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
-	char *newArray = (char *)malloc(size * sizeof(char));
+	*newArray = (char *)malloc(size * sizeof(char));
 
 	if (newArray == NULL)
 	{
 		return (NULL); /* Memory allocation failed */
 	}
 
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		newArray[i] = c; /* Initialize each element with the specified char */
 	}
