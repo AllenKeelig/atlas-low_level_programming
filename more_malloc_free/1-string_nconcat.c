3a09c1a7b3ad6d80acb2487cec022cb24d476516
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-17 02:28:00 +0000

    1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,15 +12,16 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+
+	unsigned int len1;
+        unsigned int len2;
+        unsigned int result_len;
+        char *result;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	unsigned int len1;
-	unsigned int len2;
-	unsigned int result_len;
-	char *result
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
