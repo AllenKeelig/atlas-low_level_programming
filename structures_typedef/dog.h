13f54e9973b7476c44619c65b5c51e30b5b3ae58
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-22 19:28:01 +0000

    1initialize

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,16 +2,13 @@
 #define DOG_H
 
 /**
- * struct dog - Represents information about a dog.
- * @name: Pointer to a character array representing the dog's name.
- * @age: Age of the dog (floating-point number).
- * @owner: Pointer to a character array representing the owner's name.
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
  */
-
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-
 struct dog
 {
 	char *name;
@@ -19,5 +16,18 @@ struct dog
 	char *owner;
 };
 
-#endif /* DOG_H */
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+/*headers suck to write so I just took this one bite me*/
+
+
+#endif
