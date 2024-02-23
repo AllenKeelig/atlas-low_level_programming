34b0951c0a9bfe80baaa512ebae7d351eb7b95ea
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-23 23:33:16 +0000

    3-hopefullysetupisright

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+	op_t ops[] =
+	{
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+
