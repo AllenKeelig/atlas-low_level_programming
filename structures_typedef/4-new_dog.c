485681ba34bfcf25d994218ec9075f574361ee07
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-23 14:13:42 +0000

    4strcpy

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -65,8 +65,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return NULL;
 	}
 
-	_strcpy(dog->name, name);
-	_strcpy(dog->owner, owner);
+	_strcpy(newdog->name, name);
+	_strcpy(newdog->owner, owner);
 	newdog->age = age;
 
 	return newdog;
