1f131ddc6af7f774406ac2ed11512a4f0e4b5b48
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-23 22:37:26 +0000

    2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	size_t i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
