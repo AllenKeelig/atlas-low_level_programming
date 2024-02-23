2271ec906c5f742ea373df070d83a2f0a71bb1eb
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-23 02:10:30 +0000

    2

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: A pointer to the new dog (struct dog), or NULL if the function fails.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (name == NULL || owner == NULL)
+		return NULL;
+
+	dog_t *newDog = malloc(sizeof(dog_t));
+	if (newDog == NULL)
+		return NULL;
+
+	newDog->name = strdup(name);
+	newDog->owner = strdup(owner);
+
+	if (newDog->name == NULL || newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog->owner);
+		free(newDog);
+		return NULL;
+	}
+
+	newDog->age = age;
+
+	return newDog;
+}
+
