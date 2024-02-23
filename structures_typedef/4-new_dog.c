6c7ec8ea0b42e9ed55601ec6978253eafd2a31b9
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-23 02:18:44 +0000

    4struct

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -17,7 +17,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name == NULL || owner == NULL)
 		return NULL;
 
-	dog *newDog = malloc(sizeof(dog_t));
+	struct dog *newDog = malloc(sizeof(dog_t));
 	if (newDog == NULL)
 		return NULL;
 
