0058a37a2efebb1f7ca553c94793b81c57ed420c
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-23 20:54:46 +0000

    README

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * print_name - prints a name
+ *
+ * Return: do I need this here?
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
-    if (f != NULL) {
-        f(name);
-    }
+	if (f != NULL)
+	{
+		f(name);
+	}
 }
