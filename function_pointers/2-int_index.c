903c86161d8ba4f6967ff71f13c9d9038b459ffa
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-23 22:46:43 +0000

    2betty

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
+/**
+ * int_index - searching for an integer
+ * @array: yup
+ * @size: its a size
+ * @cmp: pointer to the function compared to
+ *
+ * Return: integers location or NULL if LOL
+ */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
 	{
-		return -1;
+		return (-1);
 	}
 
 	for (i = 0; i < size; i++)
