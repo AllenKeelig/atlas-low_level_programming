b6418b95abe5a8a1d775bd136f9200fd5939a708
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-23 22:04:10 +0000

    1betty

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * array_iterator - executes a function across an array.
+ * @array: the array
+ * @size: ...
+ * @action: the function
+ *
+ */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
