29dcf5f0333307446050d41a5fc219a58617f0c8
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-24 00:24:14 +0000

    3BETTY

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -18,7 +18,7 @@ int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		(return 0);
+		return (0);
 	}
 	return (a / b);
 }
@@ -26,7 +26,7 @@ int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
-		(return 0);
+		return (0);
 	}
 	return (a % b);
 }
