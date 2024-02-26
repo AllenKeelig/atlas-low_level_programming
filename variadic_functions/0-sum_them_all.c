1e3fe7bab1be241ab53c36a30f072183e4870e12
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-26 16:15:33 +0000

    betty

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,7 +9,7 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
-        va_list args;
+	va_list args;
 	unsigned int i;
 
 	if (n == 0)
@@ -18,9 +18,8 @@ int sum_them_all(const unsigned int n, ...)
 	}
 
 	va_start(args, n);
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
-	}
 
 	va_end(args);
 
