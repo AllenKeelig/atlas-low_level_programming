cf79c3ab0b1bb30dd05d17ad642fd735ceaa5a74
AllenKeelig <allen.keeling@atlasschool.com>
2024-03-09 20:48:38 +0000

    0betty

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,9 +2,9 @@
 #define LISTS_H
 typedef struct list_s
 {
-    char *str;
-    size_t len;
-    struct list_s *next;
+	char *str;
+	size_t len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
