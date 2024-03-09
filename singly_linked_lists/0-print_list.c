ae44c1cbe98431288bc9346722eed3bc0573dc22
AllenKeelig <allen.keeling@atlasschool.com>
2024-03-09 05:00:27 +0000

    format

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		printf("[%lu] %s\n", h->str != NULL ? h->str : "(nil)");
+		printf("[%u] %s\n", h->str != NULL ? h->str : "(nil)");
 		h = h->next;
 		count++;
 	}
