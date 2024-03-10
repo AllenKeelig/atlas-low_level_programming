89d56ae84cbfdcba407d1f5ac6a4c5037e453bf8
AllenKeelig <allen.keeling@atlasschool.com>
2024-03-10 05:15:41 +0000

    3mixed

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -12,10 +12,10 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *new_node = malloc(sizeof(list_t));
 	if (head == NULL || str == NULL)
 		return (NULL);	/* Invalid input parameters */
 
-	list_t *new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);	/* Memory allocation failed */
 
