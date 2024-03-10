102eb161008aeebbddfd65ae9f664a1f27b99ea2
AllenKeelig <allen.keeling@atlasschool.com>
2024-03-10 18:25:08 +0000

    3kernel????

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -24,7 +24,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 	if (*head == NULL)
-		 *head = new_node;
+		*head = new_node;
 	else
 	{
 		listint_t *current = *head;
