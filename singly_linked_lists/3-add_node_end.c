dc4fe373b057f3fd92dcd2c6bc760fa6cb6ad144
AllenKeelig <allen.keeling@atlasschool.com>
2024-03-10 05:14:02 +0000

    3

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,45 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: Pointer to a pointer to the head of the linked list.
+ * @str: String to be duplicated and stored in the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	if (head == NULL || str == NULL)
+		return (NULL);	/* Invalid input parameters */
+
+	list_t *new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);	/* Memory allocation failed */
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);	/* Memory allocation for the string failed */
+		return (NULL);
+	}
+
+	new_node->len = 0;	/* Initialize the length to 0 for now */
+	new_node->next = NULL;	/* The new node will be the last, so its next is NULL */
+
+	if (*head == NULL)
+		*head = new_node;	/* If the list is empty, the new node becomes the head */
+	else
+	{
+		list_t *current = *head;
+
+		while (current->next != NULL)
+			current = current->next;	/* Traverse the list until the last node is reached */
+		current->next = new_node;	/* Set the next pointer of the last node to the new node */
+	}
+
+	return new_node;
+}
+
