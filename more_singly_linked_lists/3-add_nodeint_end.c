04c3ade4b99db945b5fa60a7d18dfc524c31c014
AllenKeelig <allen.keeling@atlasschool.com>
2024-03-10 18:19:00 +0000

    3

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,35 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint_end - Adds a new node at the end of a listint_t list.
+ * @head: Pointer to a pointer to the head of the listint_t list.
+ * @n: Integer value to be stored in the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+	if (head == NULL)
+	{
+		return NULL;  /* Invalid input parameter */
+	}
+	if (new_node == NULL)
+	{
+		return NULL;  /* Memory allocation failed */
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
+		 *head = new_node;
+	else
+	{
+		listint_t *current = *head;
+		while (current->next != NULL)
+			current = current->next;  /* Traverse the list until the last node is reached */
+		current->next = new_node;  /* Set the next pointer of the last node to the new node */
+	}
+	return new_node;
+}
