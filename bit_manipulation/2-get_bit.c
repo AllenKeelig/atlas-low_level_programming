d036ea09ae932cb93c58d6327387b7c8b4c6a73d
AllenKeelig <allen.keeling@atlasschool.com>
2024-03-11 02:01:40 +0000

    2betty

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -6,14 +6,14 @@
  * @n: Unsigned long int.
  * @index: Index of the bit to retrieve, starting from 0.
  *
- * Return: The value of the bit at the specified index or -1 if an error occurred.
+ * Return: The value of the bit at the specified index or -1 if an error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
 	{
-		return -1;
+		return (-1);
 	}
 
-	return (n >> index) & 1;
+	return ((n >> index) & 1);
 }
