9f19fe4054293505d23134f0db2b6747df48c1ae
AllenKeelig <allen.keeling@atlasschool.com>
2024-03-11 13:46:00 +0000

    5

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -15,7 +15,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	for (i = 0; i < j; i++)
 	{
 		if ((m & 1) != (n & 1))
-			nflips += 1;
+			flip += 1;
 		n = n >> 1;
 		m = m >> 1;
 	}
