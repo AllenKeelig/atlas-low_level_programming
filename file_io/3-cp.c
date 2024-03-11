80f1ff44073b5ef9fb1f1b5cafa0ef291fa683f2
AllenKeelig <allen.keeling@atlasschool.com>
2024-03-11 17:08:12 +0000

    3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -36,7 +36,8 @@ int main(int argc, char *argv[])
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		handle_error(98, "Error: Can't read from file", fd_from, -1);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
