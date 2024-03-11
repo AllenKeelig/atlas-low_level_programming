5ded89c8ac45e0d2875ce5c56868ee676d2bff84
AllenKeelig <allen.keeling@atlasschool.com>
2024-03-11 17:26:03 +0000

    3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -21,55 +21,62 @@ void handle_error(int code, const char *message, int fd1, int fd2)
 	dprintf(STDERR_FILENO, "%s\n", message);
 	exit(code);
 }
-
-int main(int argc, char *argv[])
+int openFile(const char *filename, int flags)
 {
-	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
-	if (argc != 3)
+	int fd = open(filename, flags);
+	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", filename);
 		exit(98);
 	}
+	return fd;
+}
 
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
-		exit(99);
-	}
+void copyFile(int fd_from, int fd_to)
+{
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
+			dprintf(STDERR_FILENO, "Error: Can't write to file\n");
 			exit(99);
 		}
 	}
 
 	if (bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file\n");
 		exit(98);
 	}
+}
 
-	if (close(fd_from) == -1 || close(fd_to) == -1)
+void closeFile(int fd)
+{
+	if (close(fd) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	int fd_from = openFile(argv[1], O_RDONLY);
+	int fd_to = openFile(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	copyFile(fd_from, fd_to);
+
+	closeFile(fd_from);
+	closeFile(fd_to);
 
 	return 0;
 }
-
