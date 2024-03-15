7721b6316be21deaad619c888e5c75ec9226d26c
AllenKeelig <allen.keeling@atlasschool.com>
2024-03-15 18:36:05 +0000

    betty?

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: Pointer to the head of the doubly linked list.
+ *
+ * Return: The number of nodes in the list.
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t node_count = 0;
