17aff1252bcad68e9d563974650917efb27b7c1e
AllenKeelig <allen.keeling@atlasschool.com>
2024-03-17 23:18:06 +0000

    3

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+
+	unsigned long int index = key_index((unsigned char *)key, ht->size);
+	hash_node_t *new_node = ht->array[index];
+
+	while (new_node != NULL)
+	{
+		if (strcmp(new_node->key, key) == 0)
+		{
+			free(new_node->value);
+			new_node->value = strdup(value);
+			if (new_node->value == NULL)
+				return (0);
+			return (1);
+		}
+		new_node = new_node->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (0);
+	}
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
