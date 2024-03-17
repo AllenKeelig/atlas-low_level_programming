1c0e8760c67dcfa5419c5af75579ab481ca80ceb
AllenKeelig <allen.keeling@atlasschool.com>
2024-03-17 23:20:14 +0000

    3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -5,12 +5,13 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
+	hash_node_t *new_node;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
-	hash_node_t *new_node = ht->array[index];
+	new_node = ht->array[index];
 
 	while (new_node != NULL)
 	{
