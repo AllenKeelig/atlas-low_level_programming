8c1ab14874f01a89c93581a006d7484ca72fcb95
AllenKeelig <allen.keeling@atlasschool.com>
2024-03-18 15:55:02 +0000

    6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -25,19 +25,4 @@ free(ht);
  * free_hash_list - frees a hash_node_t list
  * @head: head of linked list
  */
-void free_hash_list(hash_node_t *head)
-{
-	hash_node_t *current;
-	hash_node_t *next;
-
-	current = head;
-
-	while (current != NULL)
-	{
-		next = current->next;
-		free(current->key);
-		free(current->value);
-		free(current);
-		current = next;
-	}
-}
+ 
